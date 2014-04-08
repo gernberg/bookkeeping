8eb6746003c6d29334282431e96f3d79b7ab1752
Gustav Ernberg <gustav@exceed-it.se>
2014-04-08 22:33:59 +0200

    Rails project start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookie::Application.config.secret_key_base = 'e5f669c85a30e80c047ec483518dcf14a2866f7039675f303e70f335dbd29e9ccda2dd0fa695b88b9bef15eff01dbd22204bc1d5f9cf544f49952d1e27017e89'
