03c754070f4cd28e4a7870ba93609cdd3032b622
guser350 <guser350@student.liu.se>
2014-04-12 20:04:28 +0200

    Upgraded to Bootstrap 3.0

--- src/app/app.js
+++ src/app/app.js
@@ -23,6 +23,17 @@ angular.module( 'bookie', [
     }
   });
 })
+.directive('ngEnter', function() {
+  return function(scope, element, attrs) {
+    element.bind("keydown keypress", function(event) {
+      if(event.which === 13) {
+        scope.$apply(function(){
+          scope.$eval(attrs.ngEnter, {'event': event});
+        });
 
-;
+        event.preventDefault();
+      }
+    });
+  };
+});
 
