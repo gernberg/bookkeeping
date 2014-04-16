6836da06a8cadedcf467696333ba778acfd98b6d
Gustav Ernberg <gustav@exceed-it.se>
2014-04-16 23:01:26 +0200

    Tests pass

--- app/models/fiscal_year.rb
+++ app/models/fiscal_year.rb
@@ -2,4 +2,14 @@ class FiscalYear < ActiveRecord::Base
   has_paper_trail
   belongs_to :company
   validates_presence_of :company, :start_date, :end_date
+  # Only check intervals if dates are supplied
+  validate :check_year_interval, :if=>Proc.new{|fy| fy.start_date.present? && fy.end_date.present?}
+
+  def check_year_interval
+    if (start_date - end_date) < 0
+      errors.add(:start_date, :should_not_end_before_start)
+    elsif (end_date - start_date) > 18.months
+      errors.add(:end_date, :could_not_exceed_18_months)
+    end
+  end
 end
--- spec/models/fiscal_year_spec.rb
+++ spec/models/fiscal_year_spec.rb
@@ -6,15 +6,28 @@ describe FiscalYear do
   end
 
   it "could be 18 months" do
-    FactoryGirl.build(:fiscal_year, {start_date: 18.months.ago, end_date: Date.new}).should be_valid
+    FactoryGirl.build(:fiscal_year, {start_date: Time.new, end_date: 18.months.ago}).should be_valid
   end
 
   it "must not be longer than a 18 months" do
-    FactoryGirl.build(:fiscal_year, {start_date: 19.months.ago, end_date: Date.new}).should_not be_valid
+    FactoryGirl.build(:fiscal_year, {start_date: 19.months.ago, end_date: Time.now}).should_not be_valid
   end
 
   it "must belong to a company" do
     FactoryGirl.build(:fiscal_year, {company: nil}).should_not be_valid
+  end
+
+  it "must have a start date" do
+    FactoryGirl.build(:fiscal_year, {start_date: nil}).should_not be_valid
+  end
+
+  it "must have a end date" do
+    FactoryGirl.build(:fiscal_year, {end_date: nil}).should_not be_valid
+  end
+
+  it "must not end before it starts" do
+    puts "must not end"
+    FactoryGirl.build(:fiscal_year, {start_date: 1.year.ago.year, end_date: Time.now.year}).should_not be_valid
   end
 
 end
