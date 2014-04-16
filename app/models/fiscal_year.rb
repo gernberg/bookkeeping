4872d9214f32f9bda8a4c55e095843c6f8a9c9e2
Gustav Ernberg <gustav@exceed-it.se>
2014-04-16 23:04:50 +0200

    More text

--- app/models/fiscal_year.rb
+++ app/models/fiscal_year.rb
@@ -2,9 +2,13 @@ class FiscalYear < ActiveRecord::Base
   has_paper_trail
   belongs_to :company
   validates_presence_of :company, :start_date, :end_date
+  
   # Only check intervals if dates are supplied
   validate :check_year_interval, :if=>Proc.new{|fy| fy.start_date.present? && fy.end_date.present?}
 
+
+  ## 
+  # Checks that year conforms to swedish regulations
   def check_year_interval
     if (end_date - start_date) > 0
       errors.add(:start_date, :should_not_end_before_start)
