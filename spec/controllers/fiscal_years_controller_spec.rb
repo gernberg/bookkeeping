fafde10acc08cf2eef5a4b4f64b4ec846fd4d7d2
Gustav Ernberg <gustav@exceed-it.se>
2014-04-17 00:59:34 +0200

    Passing tests

--- spec/controllers/fiscal_years_controller_spec.rb
+++ spec/controllers/fiscal_years_controller_spec.rb
@@ -31,10 +31,10 @@ describe FiscalYearsController do
 
     it "edit companies fiscal years" do
       expect{
-        put :update, id:@fiscal_year, company_id: @user_company.id, fiscal_year: {start_date: 10.years.ago, end_date: 9.years.ago}
+        put :update, id:@fiscal_year, company_id: @user_company.id, fiscal_year: {start_date: 8.years.ago, end_date: 7.years.ago}
         @fiscal_year.reload
         puts @fiscal_year.inspect
-      }.to change(@fiscal_year, :start_date).to(10.years.ago.year)
+      }.to change(@fiscal_year, :start_date).to(8.years.ago.to_date)
     end
     it "shows companies fiscal year" do
       get :show, company_id: @user_company.id, id: @fiscal_year
