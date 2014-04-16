cf62eea4d199bca0d33fbb8a09a49ad135231a07
Gustav Ernberg <gustav@exceed-it.se>
2014-04-16 23:01:44 +0200

    Tests pass

--- app/models/fiscal_year.rb
+++ app/models/fiscal_year.rb
@@ -6,7 +6,7 @@ class FiscalYear < ActiveRecord::Base
   validate :check_year_interval, :if=>Proc.new{|fy| fy.start_date.present? && fy.end_date.present?}
 
   def check_year_interval
-    if (start_date - end_date) < 0
+    if (end_date - start_date) > 0
       errors.add(:start_date, :should_not_end_before_start)
     elsif (end_date - start_date) > 18.months
       errors.add(:end_date, :could_not_exceed_18_months)
