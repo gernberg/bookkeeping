b6eaddf16a95459f88351163e088baa4022ebd8f
Gustav Ernberg <gustav@exceed-it.se>
2014-04-19 18:43:02 +0200

    More passing tests

---
+++ spec/controllers/vouchers_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe VouchersController do
+
+  before :each do
+    @random_company = FactoryGirl.create(:company)
+    @user_company = FactoryGirl.create(:company)
+    @fiscal_year = FactoryGirl.create(:fiscal_year, company: @user_company)
+    @random_fiscal_year = FactoryGirl.create(:fiscal_year, company: @random_company)
+
+
+    @user = FactoryGirl.create(:user) 
+    @user.companies = [@user_company]
+  end
+  describe "signed in" do
+    before :each do 
+      sign_in @user 
+    end
+    it "returns only the current companys fiscal years vouchers" do
+      get "index", :company_id => @user_company.id, :fiscal_year_id => @fiscal_year.id
+      response.should be_success
+      assigns(:vouchers).count.should eql(@fiscal_year.vouchers.count)
+      assigns(:vouchers).to_a.should match_array(@fiscal_year.vouchers.to_a)
+    end
+  end
+  describe "not signed in" do
+
+  end
+end
--- spec/models/voucher_spec.rb
+++ spec/models/voucher_spec.rb
@@ -4,12 +4,15 @@ describe Voucher do
   it "has a valid factory" do
     FactoryGirl.build(:voucher).should be_valid
   end
+
   it "needs title to save" do
     FactoryGirl.build(:voucher, title: nil).should_not be_valid
   end
+
   it "needs date to save" do
     FactoryGirl.build(:voucher, date: nil).should_not be_valid
   end
+
   it "accepts valid date" do
     FactoryGirl.build(:voucher, date: Date.new(Time.now.year)).should be_valid
   end
@@ -21,18 +24,27 @@ describe Voucher do
   it "belongs to a fiscal year" do
     FactoryGirl.build(:voucher, fiscal_year: nil).should_not be_valid
   end
-  it "valid inside fiscal year" do
+  
+  it "is valid inside fiscal year" do
     @fiscal_year = FactoryGirl.create(:fiscal_year, :start_date => 2.years.ago, :end_date => 1.year.ago);
     FactoryGirl.build(:voucher, fiscal_year: @fiscal_year, date: 18.months.ago).should be_valid
   end
-  it "invalid after fiscal year" do
+  
+  it "is invalid after fiscal year" do
     @fiscal_year = FactoryGirl.create(:fiscal_year, :start_date => 2.years.ago, :end_date => 1.year.ago);
     FactoryGirl.build(:voucher, fiscal_year: @fiscal_year, date: 3.years.ago).should_not be_valid
   end
-  it "invalid before fiscal year" do
+  
+  it "is invalid before fiscal year" do
     @fiscal_year = FactoryGirl.create(:fiscal_year, :start_date => 2.years.ago, :end_date => 1.year.ago);
     FactoryGirl.build(:voucher, fiscal_year: @fiscal_year, date: 11.months.ago).should_not be_valid
     FactoryGirl.build(:voucher, fiscal_year: @fiscal_year, date: 1.months.ago).should_not be_valid
     FactoryGirl.build(:voucher, fiscal_year: @fiscal_year, date: 1.months.ago).should_not be_valid
   end
+
+  it "gets a sequential number" do
+    @voucher = FactoryGirl.create(:voucher)
+    @voucher.number.should eq(1)
+  end
+
 end
