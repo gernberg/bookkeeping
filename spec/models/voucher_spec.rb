186ad858d90f53a108af469c5dc379ef55a0f57f
Gustav Ernberg <gustav@exceed-it.se>
2014-04-19 18:24:36 +0200

    Basic tests for vouchers

--- spec/models/voucher_spec.rb
+++ spec/models/voucher_spec.rb
@@ -4,7 +4,12 @@ describe Voucher do
   it "has a valid factory" do
     FactoryGirl.build(:voucher).should be_valid
   end
-  pending "has a valid account"
+  it "needs title to save" do
+    FactoryGirl.build(:voucher, title: nil).should_not be_valid
+  end
+  it "needs date to save" do
+    FactoryGirl.build(:voucher, date: nil).should_not be_valid
+  end
   pending "belongs to a fiscal year"
   pending "Is inside fiscal year"
 end
