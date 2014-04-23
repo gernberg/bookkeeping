9dc4572a1db45d9a69073142aa80d3ac4abc6f62
Gustav Ernberg <gustav@exceed-it.se>
2014-04-23 19:23:54 +0200

    Voucher nested save started -- unit testing needed

--- src/app/voucher/vouchers.js
+++ src/app/voucher/vouchers.js
@@ -60,7 +60,7 @@ angular.module( 'bookie.voucher', [
   $scope.sumCredit = function(voucher){
     var sum = 0;
     for(var i = 0; i<voucher.voucher_rows.length; i++){
-      add = parseFloat(voucher.voucher_rows[i].debit, 10);
+      add = parseFloat(voucher.voucher_rows[i].credit, 10);
       if(!isNaN(add)){
         sum += add;
       }
@@ -120,7 +120,28 @@ angular.module( 'bookie.voucher', [
     alert("DELETE");
   };
 
+  $scope.findAccountId = function(account_number){
+    for(var i = 0; i < $scope.accounts.length; i++){
+      if($scope.accounts[i].account_number == account_number){
+        return $scope.accounts[i].id;
+      }
+    }
+  };
+
   $scope.submit = function(){
+    if(true){
+      $scope.voucher.voucher_rows_attributes = [];
+      for(var i = 0; i < $scope.voucher.voucher_rows.length; i++){
+        $scope.voucher.voucher_rows_attributes.push(
+            {
+              account_id: $scope.findAccountId($scope.voucher.voucher_rows[i].account),
+          credit: $scope.voucher.voucher_rows[i].credit,
+          debit: $scope.voucher.voucher_rows[i].debit
+            });
+      }
+      $scope.voucher.$save();
+      return false;
+    }
     if($scope.voucherId){
       $scope.voucher.$update(function(response){
         $state.transitionTo('vouchers');
@@ -145,52 +166,52 @@ angular.module( 'bookie.voucher', [
 .factory( 'VoucherRes', function ( $resource, CompanyService, FiscalService, VoucherCache)  {
   return $resource('../companies/:cid/fiscal_years/:fid/vouchers/:id.json', {fid: FiscalService.currentFiscalYearId(), cid: CompanyService.currentCompanyId(), id:'@id'}, {
     'query' : {method: 'GET', isArray: true,
-              cache: VoucherCache},
-    'update': {method: 'PATCH'}
-    
+      cache: VoucherCache},
+         'update': {method: 'PATCH'}
+
   });
 })
 .directive('currencyInput', function($filter, $browser) {
-    return {
-        require: 'ngModel',
-        link: function($scope, $element, $attrs, ngModelCtrl) {
-            var listener = function() {
-                var value = $element.val().replace(/,/g, '.');
-                value = value.replace(/(\d*\.\d\d)(.*)/, '$1');
-                //$element.val($filter('number')(value, false));
-                $element.val(value);
-            };
-            
-            // This runs when we update the text field
-            ngModelCtrl.$parsers.push(function(viewValue) {
-                return viewValue.replace(/,/g, '');
-            });
-            
-            // This runs when the model gets updated on the scope directly and keeps our view in sync
-            ngModelCtrl.$render = function() {
-                $element.val($filter('number')(ngModelCtrl.$viewValue, false));
-            };
-            
-            $element.bind('change', listener);
-            $element.bind('keydown', function(event) {
-                var key = event.keyCode;
-                // If the keys include the CTRL, SHIFT, ALT, or META keys, or the arrow keys, do nothing.
-                // This lets us support copy and paste too
-                if (key == 91 || (15 < key && key < 19) || (37 <= key && key <= 40)){
-                    return ;
-                }
-                if (event.keyCode == 188)
-            {
-            }
-                  
-                $browser.defer(listener); // Have to do this or changes don't get picked up properly
-            });
-            
-            $element.bind('paste cut', function() {
-                $browser.defer(listener);
-            });
-        }
-    };
+  return {
+    require: 'ngModel',
+link: function($scope, $element, $attrs, ngModelCtrl) {
+  var listener = function() {
+    var value = $element.val().replace(/,/g, '.');
+    value = value.replace(/(\d*\.\d\d)(.*)/, '$1');
+    //$element.val($filter('number')(value, false));
+    $element.val(value);
+  };
+
+  // This runs when we update the text field
+  ngModelCtrl.$parsers.push(function(viewValue) {
+    return viewValue.replace(/,/g, '');
+  });
+
+  // This runs when the model gets updated on the scope directly and keeps our view in sync
+  ngModelCtrl.$render = function() {
+    $element.val($filter('number')(ngModelCtrl.$viewValue, false));
+  };
+
+  $element.bind('change', listener);
+  $element.bind('keydown', function(event) {
+    var key = event.keyCode;
+    // If the keys include the CTRL, SHIFT, ALT, or META keys, or the arrow keys, do nothing.
+    // This lets us support copy and paste too
+    if (key == 91 || (15 < key && key < 19) || (37 <= key && key <= 40)){
+      return ;
+    }
+    if (event.keyCode == 188)
+  {
+  }
+
+  $browser.defer(listener); // Have to do this or changes don't get picked up properly
+  });
+
+  $element.bind('paste cut', function() {
+    $browser.defer(listener);
+  });
+}
+};
 })
 .directive('autoComplete', function($timeout) {
   var substringMatcher = function(accounts) {
@@ -226,7 +247,7 @@ angular.module( 'bookie.voucher', [
       displayKey: 'value',
       source: substringMatcher(scope[iAttrs.uiItems]),
       templates:{
-         suggestion: Handlebars.compile('<p><strong>{{value}}</strong> – {{name}}</p>')
+        suggestion: Handlebars.compile('<p><strong>{{value}}</strong> – {{name}}</p>')
       }
 
     });
