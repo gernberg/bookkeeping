a51ed81b655866c7c6e7de770312ab6a546bd04c
Gustav Ernberg <gustav@exceed-it.se>
2014-05-10 12:27:03 +0200

    Tests for voucher rows

--- spec/factories/voucher.rb
+++ spec/factories/voucher.rb
@@ -3,5 +3,9 @@ FactoryGirl.define do
     fiscal_year {FactoryGirl.build(:fiscal_year)}
     title "Randomtext"
     date Date.new(Time.now.year)
+    voucher_rows {[
+      FactoryGirl.build(:voucher_row, debit: 100),
+      FactoryGirl.build(:voucher_row, credit: -100)
+    ]}
   end
 end
